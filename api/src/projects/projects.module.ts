eec4f99fdf371b9e2563535e0d9391804ec2396d
indeep99 <indeep.singh99@gmail.com>
2021-10-18 01:34:01 -0400

    added service and controller for project

--- api/src/projects/projects.module.ts
+++ api/src/projects/projects.module.ts
@@ -3,12 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
 import { ProjectEntryEntity } from './model/project-entry.entity';
+import { ProjectController } from './controller/project.controller';
+import { ProjectService } from './service/project.service';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([ProjectEntryEntity]),
         AuthModule,
         UserModule
-    ]
+    ],
+    controllers: [ProjectController],
+    providers: [ProjectService]
 })
 export class ProjectsModule {}
